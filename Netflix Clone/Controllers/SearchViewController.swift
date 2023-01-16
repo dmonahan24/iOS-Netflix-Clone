95896802eac20641e1456d26fe17ecd5d74c6547
Dave Monahan <dmonahan24@gmail.com>
2023-01-16 17:21:36 -0500

    initial commit

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Dave Monahan on 1/16/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+
+    }
+    
+
+
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Dave Monahan on 1/16/23.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+
+}
